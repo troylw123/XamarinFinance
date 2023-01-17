6017f96258df463f61186819329fd0b77a20f3a6
Troy Weaver <troylw@gmail.com>
2023-01-17 14:41:07 -0500

    Add project files.

---
+++ Finance/ViewModel/MainVM.cs
@@ -0,0 +1,44 @@
+﻿using Finance.Model;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Finance.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public Posts Blog
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM()
+        {
+            ReadRss();
+        }
+
+        public void ReadRss()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+
+            using (WebClient client = new WebClient())
+            {
+                string xml = Encoding.Default.GetString(client.DownloadData("https://cointelegraph.com/rss"));
+                using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                {
+                    Blog = (Posts)serializer.Deserialize(reader);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
